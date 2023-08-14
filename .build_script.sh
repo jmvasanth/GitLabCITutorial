3fab9e43e5d9bf09a93044f9c3eb6179517e7678
jmvasanth <39864718+jmvasanth@users.noreply.github.com>
2023-08-14 22:51:57 +1000

    Update .build_script.sh

--- .build_script.sh
+++ .build_script.sh
@@ -2,7 +2,7 @@
 #update your token and git url in the clone command below
 #update your s3 bucket name
 
- git clone -b develop  https://gitlab+deploy-token-497024:$TOKEN@gitlab.com/piyush.sachdeva055/myfirstproject.git
+ git clone -b develop  https://gitlab+deploy-token-497024:$CI_JOB_TOKEN@gitlab.com/jmvasanth/GitLabCITutorial.git
  cd myfirstproject
  npm install express --save
  cd ../
